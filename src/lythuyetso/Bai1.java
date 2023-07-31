baa556435474542c528c366e5237a3078618751f
Thuc Vu <vuthuc3152001@gmail.com>
2023-07-31 10:43:26 +0700

    31/07/2023

---
+++ src/lythuyetso/Bai1.java
@@ -0,0 +1,7 @@
+package lythuyetso;
+
+public class Bai1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/lythuyetso/Bai2.java
@@ -0,0 +1,12 @@
+package lythuyetso;
+
+import java.util.Scanner;
+
+public class Bai2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        System.out.println(n);
+    }
+}
